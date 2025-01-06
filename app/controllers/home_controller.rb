9c449e04902fe7316e4dfebddc8709928c3c0c99
ROCIO MARQUEZ <rociomarquez@uc.cl>
2025-01-06 14:28:57 -0300

    commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+    def index
+        # Aquí puedes agregar la lógica para tu acción, como cargar datos desde la base de datos
+      end
+end
---
+++ app/views/home/index.html.erb
@@ -0,0 +1,1 @@
+<h1>Bienvenido a la página de inicio</h1>
